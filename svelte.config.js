201bdb115d4916b24bde46667cc4c2a95a39ecf4
tom-2810 <tom.poortman@hva.nl>
2023-12-07 23:24:14 +0100

    revert

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	}
 };
-
-export default config;
