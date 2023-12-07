35875c8acb8285799c74ff03279b5466c614418c
tom-2810 <tom.poortman@hva.nl>
2023-12-07 22:53:55 +0100

    workflow test

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
+
+export default config;
